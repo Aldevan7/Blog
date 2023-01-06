175f82b0ea298f6b7cd6841b457f663052eb4ff6
Aldevan Santos <aldevan.santos@gmail.com>
2023-01-06 20:38:47 -0300

    Creating the structure of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 
+import Header from './components/Header/index.jsx'
+import Article from './components/Article'
+
 
 function App() {
 
   return (
+    <>    
+    <Article/>
+    <Header/>
     <h1>Meu blog</h1>
+    </>
   )
 }
 
---
+++ src/components/Article/index.jsx
@@ -0,0 +1,12 @@
+import Posts from '../Posts'
+
+function Article(){
+  return(
+    <>
+    <h1>Article</h1>
+    <Posts/>
+    </>
+  )  
+}
+
+export default Article
